f42b1d42c29b5fe5b7bda7068f238d4703215823
tranhieuamg <tranhieuamg@yahoo.com>
2026-03-18 23:12:35 +0700

    Update hftest1.py

--- hftest1.py
+++ hftest1.py
@@ -126,7 +126,7 @@ if st.session_state.df is not None:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if prompt := st.chat_input("Summarize those posts by answering these questions: 1. what are the topics being discussed? 2. what are the key points being made in each topic? 3. what brands and products are being mentioned? What are community's opinion about those brands or products? 4. Please plot a frequency bar plot of the mentioned products")
+    if prompt := st.chat_input("Summarize those posts by answering these questions: 1. what are the topics being discussed? 2. what are the key points being made in each topic? 3. what brands and products are being mentioned? What are community's opinion about those brands or products? 4. Please plot a frequency bar plot of the mentioned products"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
